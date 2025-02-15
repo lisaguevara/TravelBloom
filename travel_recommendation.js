bb9357c607e625f233878c4fd4489bc726ed7fcb
Lisa Guevara <Gueval3877@gmail.com>
2025-02-15 13:59:19 -0600

    Display results & comment cleanup

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,40 @@
+function displayResults(result){
+    result.forEach((item, i, arr) => {
+         if (item.cities) {
+            item.cities.forEach(city => createResultElems(city));
+        } else {
+            createResultElems(item);
+        }  
+    });
+}
+
+function createResultElems(result){
+    const resultsElem = document.getElementById("searchResults");
+    resultsElem.classList.remove("hide");
+    resultsElem.classList.add("show");
+    let divElem = document.createElement("div");
+    divElem.id = result.id;
+    divElem.classList.add("result");
+
+    let nameElem = document.createElement("p");
+    nameElem.classList.add("cityName");
+    nameElem.textContent = result.name;
+    divElem.append(nameElem);
+
+    let imgElem = document.createElement("img");
+    imgElem.classList.add("cityImg");
+    imgElem.alt = result.name;
+    imgElem.src = result.imageUrl;
+    divElem.append(imgElem);
+
+    let descElem = document.createElement("p");
+    descElem.classList.add("cityDesc");
+    descElem.textContent = result.description;
+    divElem.append(descElem);
+
+    resultsElem.append(divElem);
+}
+
 function getRecommendation(e){
     e.preventDefault();
     const emptySearchMsg = "Empty...bad job!";
@@ -6,7 +43,6 @@ function getRecommendation(e){
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
         // Pull input search text and normalize/validate
         // Ideally, I'd also remove any html in the search text here...
         let searchTextRaw = document.querySelector('form#search input').value.trim();
@@ -18,9 +54,8 @@ function getRecommendation(e){
         // Compare input text with object names
         let searchResults = tempResults.filter(objName => {return objName.includes(searchText)});
         if (searchResults.length > 0) { // If text contains object name, show obj contents
-            console.log("Getting warmer");
             searchResults.forEach((item, i, arr) => {
-                console.log(data[item]);
+                displayResults(data[item]);
             });
         } else { // If not, say sorry try again
             console.log("ice cold...");
