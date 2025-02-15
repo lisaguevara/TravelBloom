4b1347371a3cc7226fe2479a76e977dbcfd902f3
Lisa Guevara <Gueval3877@gmail.com>
2025-02-14 22:38:34 -0600

    Start fetch process

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,6 +2,19 @@ function getRecommendation(e){
     e.preventDefault();
 
     let url = "./travel_recommendation_api.json";
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        // Pull input search text and normalize/validate
+        // Compare input text with object names
+        // If text contains object name, show obj contents
+            // If not, say sorry try again
+
+    })
+    .catch(er => {
+        console.log(er => console.log("Fetch Error: " + er));
+    });
 }
 
 const searchForm = document.getElementById('search');
