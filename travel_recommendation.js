42950cdb18539225628f7af076d6423867670505
Lisa Guevara <Gueval3877@gmail.com>
2025-02-14 22:23:26 -0600

    Set e.listener + listener function

---
+++ travel_recommendation.js
@@ -0,0 +1,10 @@
+function getRecommendation(e){
+    e.preventDefault();
+
+    let url = "./travel_recommendation_api.json";
+}
+
+const searchForm = document.getElementById('search');
+searchForm.addEventListener("submit", getRecommendation);
+
+
